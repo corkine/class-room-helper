ee272320ac5c191f216cf450f78da73899f1d086
Corkine <corkine@outlook.com>
2020-05-13 22:13:03 +0800

    重构代码，更符合 JavaFx 风格

--- README.md
+++ README.md
@@ -11,3 +11,15 @@
 ![](http://static2.mazhangjing.com/badge/openjdk.png)
 ![](http://static2.mazhangjing.com/badge/javafx.png)
 ![](http://static2.mazhangjing.com/badge/scala.png)
+
+## 注意
+
+这个小工具是我对于 JavaFx Property 和 Binding 风格 MVC 开发的一些思考，吸纳了 Scala mixin 风格代码特点：
+
+写出这样的代码很难，反直觉，但遵循如下步骤可以做到：
+
+1. 将样式和结构分离 CSS 和 UI 组件分离
+
+2. 将视图和动作分离，分离时优先使用 Property 而非 Controller Method，引起其能够尽可能地降低 Controller 和 View 的各自对对方的依赖关系（Controller 中使用过多 View 组件引用，导致组件全部必须暴露和作为类变量，而不能像 Flutter/ScalaFx 那样按照结构良好的嵌套，对于大工程而言，铺平组件没有嵌套组件容易读；View 中调用过多 Controller 方法，因为 View 调用基本嵌套在组件中，因此很难寻找，如果 Controller 方法修改，除了 IDE 的自动重构，寻找其在 View 的调用非常困难，而 Property 则远少于 UI 组件，因此其放在类变量中，平整易读，对于 View 和 Controller 的修改都更清晰）。
+
+ps. 这种分离很常见，react.js 和 flutter 使用的是对状态进行控制，只允许某种方法来更新状态，其不灵活，而 JavaFx 的 Property 本身是值而不是变量，Binding 本质是 Listener，因此能力较灵活，虽然丧失了一些数据变化流动性观察的入口，但整体而言，代码结构很清晰，值得选择。
---
+++ src/main/scala/app.scala
@@ -0,0 +1,156 @@
+import java.io.File
+import java.util
+
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.beans.property.{BooleanProperty, IntegerProperty, ObjectProperty, StringProperty}
+import scalafx.scene.Scene
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control._
+import scalafx.scene.layout.StackPane
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+import scala.util.Random
+
+trait ClassRoom {
+  private val history = ArrayBuffer[String]()
+  protected val students: Array[String]
+  protected def chooseSimple:String = {
+    if (students.length == 0) throw new IllegalStateException("数组为空")
+    students(Random.nextInt(students.length))
+  }
+  protected def chooseHistory:String = {
+    if (students.length == 0) throw new IllegalStateException("数组为空")
+    if (history.length == students.length) throw new IllegalStateException("没有新数据")
+    val stu = students(Random.nextInt(students.length))
+    if (!history.contains(stu)) {
+      history.append(stu); stu
+    } else chooseHistory
+  }
+  def reset():Unit = history.clear()
+  def status():(Int,Int) = students.length -> history.length
+  def choose(useHistory:Boolean):String = if (useHistory) chooseHistory else chooseSimple
+}
+
+trait Bridge {
+  //0 NODATA 1 NORMAL 2 CHANGED
+  val status: IntegerProperty = IntegerProperty(0)
+  val classRoom = new ObjectProperty[ClassRoom]()
+  val useHistory: BooleanProperty = BooleanProperty(true)
+  val nowLabel: StringProperty = StringProperty("???")
+  val currentHeader: StringProperty = StringProperty("")
+}
+
+
+object SimpleApp extends JFXApp with Bridge with Controller {
+  stage = new PrimaryStage {
+    title <== when(status === 0) choose "拖拽数据开始抽取" otherwise
+        (when(status === 2) choose "数据发生变化，点击抽取" otherwise currentHeader)
+    scene = new Scene(400,300) {
+      root = new StackPane {
+        id = "stackPane"
+        children = Seq(new StackPane {
+            val label: Label = new Label {
+              text <==> nowLabel
+              tooltip = "Click to Choose, Drag to add student list"
+              styleClass.append("info_label")
+              contextMenu = new ContextMenu(
+                new MenuItem("Reset Passed People") { onAction = _ => status.set(2) },
+                new CheckMenuItem("Use History Mode") { selected <==> useHistory }
+              )
+            }
+            children = Seq(label)
+            onMouseEntered = _ =>  label.id = "onMouse_label"
+            onMouseExited = _ => label.id = ""
+            onMouseClicked = e => {
+              if (!e.isPopupTrigger) {
+                if (nowLabel() != "???") chooseOne()
+                else showAlert(content = "请先添加数据，任意可读的文本文件，按照行分割，行第一个字符为 # 表示忽略此行。")()
+              }
+            }
+            onDragEntered = _ => {
+              label.id = "onDragEntered_label"
+              nowLabel.set("+")
+            }
+            onDragOver = e => e.acceptTransferModes(e.getTransferMode)
+            onDragDropped = e => {
+              val dragboard = e.getDragboard
+              if (dragboard.hasFiles) {
+                readFiles(dragboard.getFiles) match {
+                  case Left(e) => showAlert(content = e) {
+                      label.id = ""
+                      status.set(0)
+                    }
+                  case Right(value) =>
+                    classRoom.set(value)
+                    status.set(1)
+                }
+              }
+              e.setDropCompleted(true)
+            }
+          })
+      }
+      onShown = _ => {
+        stylesheets.add("app.css")
+        useHistory.onChange(status.set(2))
+        status.addListener { (_,_,c) =>
+          if (c == 2) { if (classRoom.get() != null) classRoom.get().reset() }
+          else if (c == 1) { nowLabel.set("开始") }
+          else if (c == 0) { nowLabel.set("???") }
+        }
+      }
+    }
+  }
+}
+
+trait Controller { self: Bridge =>
+  def readFiles(files:util.List[File]): Either[String,ClassRoom] = {
+    try {
+      val ab = new ArrayBuffer[String]()
+      files.forEach { i =>
+        val source = Source.fromFile(i,"utf-8")
+        source.getLines().toArray.foreach(i => {
+          if (!i.trim.startsWith("#") && i.nonEmpty) ab.append(i)
+        })
+        source.close()
+      }
+      Right(new ClassRoom {
+        override val students: Array[String] = ab.toArray
+      })
+    } catch {
+      case e: Throwable =>
+        println(e.getMessage)
+        Left(e.getMessage)
+    }
+  }
+  @inline def showAlert(head:String = "注意",content:String = "注意")(op: => Unit): Unit = {
+    new Alert(AlertType.Warning) {
+      headerText = head
+      contentText = content
+    }.showAndWait()
+    op
+  }
+  @inline def chooseOne(): Unit = try {
+    status.set(1)
+    val room = classRoom.get()
+    nowLabel.set(room.choose(useHistory()))
+    val (all, now) = room.status()
+    if (useHistory()) {
+       currentHeader.set(s"Passed $now / Total $all [History Mode]")
+    } else currentHeader.set(s"Total $all [Simple Mode]")
+  } catch {
+    case illegalStateException: IllegalStateException =>
+      new Alert(AlertType.Error) {
+        headerText = "错误"
+        contentText = illegalStateException.getMessage + ", 是否重置计数器？"
+        buttonTypes = Seq(ButtonType.OK, ButtonType.Cancel)
+      }.showAndWait() match {
+        case Some(ButtonType.OK) =>
+          classRoom.get().reset(); status.set(2)
+        case _ =>
+      }
+  }
+}
+
